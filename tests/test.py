cb20820d45364ab8fb92de950062336963eeaab9
David Vasquez <davaqz@gmail.com>
2017-03-28 12:21:44 -0700

    Add initial unit test file

---
+++ tests/test.py
@@ -0,0 +1,34 @@
+# project/test.py
+
+import unittest
+
+from gameapp import app
+
+class ProjectTests(unittest.TestCase):
+	##########################
+	####setup and teardown####
+	##########################
+
+	# executed prior to each test
+	def setUp(self):
+		app.config['TESTING'] = True
+		app.config['DEBUG'] = False
+		self.app = app.test_client()
+
+		self.assertEquals(app.debug, False)
+
+	# executed after each test
+	def tearDown(self):
+		pass
+
+	def test_main_page(self):
+		response = self.app.get('/', follow_redirects=True)
+		self.assertIn(b'Welcome to the Gameboy Club!', response.data)
+		self.assertIn(b'This site will contain information about the Nintendo Gameboy handheld system!', response.data)
+		self.assertIn(b'Systems', response.data)
+		self.assertIn(b'Cartridges', response.data)
+		self.assertIn(b'Accessories', response.data)
+		self.assertIn(b'Books', response.data)
+
+if __name__ == "__main__":
+	unittest.main()
