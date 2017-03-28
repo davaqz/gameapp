50bb17740d356173611574f8b263f0603f4c230f
David Vasquez <davaqz@gmail.com>
2017-03-27 18:18:55 -0700

    Initial version of Gameboy Club web application

---
+++ run.py
@@ -0,0 +1,11 @@
+# run.py
+
+# Add your settings here...this is a temporary locaiton, as the settings for a Flask app
+# should be stored separate from your main program.
+DEBUG = True
+
+from gameapp import app
+
+if __name__ == "__main__":
+	app.run()
+
