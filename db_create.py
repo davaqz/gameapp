8b68171bf1003fc4602f51cd837e5532f0f94c4e
David Vasquez <davaqz@gmail.com>
2017-04-10 18:23:58 -0700

    Added password hashing

--- db_create.py
+++ db_create.py
@@ -1,5 +1,5 @@
 from gameapp import db
-from gameapp.models import Game
+from gameapp.models import Game, User
 
 # drop all of the existing database tables
 db.drop_all()
@@ -16,4 +16,15 @@ db.session.add(game2)
 db.session.add(game3)
 
 # commit the changes
+db.session.commit()
+
+# insert user data
+user1 = User('davaqz@gmail.com', 'password1234')
+user2 = User('davaqz@hotmail.com', 'PaSsWoRd')
+user3 = User('davaqz@yahoo.com', 'MyFavPassword')
+db.session.add(user1)
+db.session.add(user2)
+db.session.add(user3)
+ 
+# commit the changes for the recipes
 db.session.commit()
