e87b293b288c2b7f0c7407f45b38cd98f96291e7
David Vasquez <davaqz@gmail.com>
2017-04-27 18:22:42 -0700

    Added confirming email address

--- gameapp/users/views.py
+++ gameapp/users/views.py
@@ -9,6 +9,8 @@ from sqlalchemy.exc import IntegrityError
 from flask_login import login_user, current_user, login_required, logout_user
 from flask_mail import Message
 from threading import Thread
+from itsdangerous import URLSafeTimedSerializer
+from datetime import datetime
 
 from .forms import RegisterForm, LoginForm
 from gameapp import db, mail, app
@@ -29,12 +31,25 @@ def send_async_email(msg):
     with app.app_context():
         mail.send(msg)
 
-def send_email(subject, recipients, text_body, html_body):
+def send_email(subject, recipients, html_body):
     msg = Message(subject, recipients=recipients)
-    msg.body = text_body
     msg.html = html_body
     thr = Thread(target=send_async_email, args=[msg])
     thr.start()
+
+def send_confirmation_email(user_email):
+    confirm_serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+
+    confirm_url = url_for(
+        'users.confirm_email',
+        token=confirm_serializer.dumps(user_email, salt='email-confirmation-salt'),
+        _external=True)
+
+    html = render_template(
+        'email_confirmation.html',
+        confirm_url=confirm_url)
+
+    send_email('Confirm Your Email Address', [user_email], html)	    
 
 ##############
 ####routes####
@@ -51,12 +66,8 @@ def register():
                 db.session.add(new_user)
                 db.session.commit()
 
-                send_email('Register',
-                    ['davaqz@gmail.com'],
-                    'Thanks for registering with Nintendo Gameboy Club!',
-                    '<h3>Thanks for registering with Nintendo Gameboy Club</h3>')
-
-                flash('Thanks for registering!', 'success')
+                send_confirmation_email(new_user.email)
+                flash('Thanks for registering! Please check your email to confirm your email address.', 'success')
                 return redirect(url_for('games.index'))
             except IntegrityError:
                 db.session.rollback()
@@ -90,4 +101,26 @@ def logout():
     db.session.commit()
     logout_user()
     flash('Goodbye!', 'info')
-    return redirect(url_for('users.login'))                    
+    return redirect(url_for('users.login'))     
+
+@users_blueprint.route('/confirm/<token>')
+def confirm_email(token):
+    try:
+        confirm_serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+        email = confirm_serializer.loads(token, salt='email-confirmation-salt', max_age=3600)
+    except:
+        flash('The confirmation link is invalid or has expired.', 'error')
+        return redirect(url_for('users.login'))
+
+    user = User.query.filter_by(email=email).first()
+
+    if user.email_confirmed:
+        flash('Account already confirmed. Please login.', 'info')
+    else:
+        user.email_confirmed = True
+        user.email_confirmed_on = datetime.now()
+        db.session.add(user)
+        db.session.commit()
+        flash('Thank you for confirming your email address!', 'success')
+
+    return redirect(url_for('games.index'))                      
